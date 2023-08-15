076a9201e379503b7297cb9be2c522d3abea836e
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-08-15 15:49:38 -0400

    removed dist folder

--- my-app/src/Hero.jsx
+++
@@ -1,9 +0,0 @@
-export default function Hero()
-{
-    return <>
-        <header className="hero">
-            <h1>Welcome!</h1>
-            <p>My name is Alexander Solecki and I'm a software developer & designer</p>
-        </header>
-    </>
-}
